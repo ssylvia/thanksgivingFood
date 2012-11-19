4e79f3c408ff7605330bd23a01d777dd2623a8b8
Stephen Sylvia <ssylvia@esri.com>
2012-11-19 15:28:48 -0500

    new code layout fixed IE bug

--- javascript/layout.js
+++ javascript/layout.js
@@ -1,205 +1,107 @@
-  dojo.require("esri.map");
-  dojo.require("esri.dijit.Legend");
-  dojo.require("esri.dijit.Scalebar");
-  dojo.require("esri.arcgis.utils");
-  dojo.require("esri.IdentityManager");
-  dojo.require("dijit.dijit");
-  dojo.require("dijit.layout.BorderContainer");
-  dojo.require("dijit.layout.ContentPane");
-  dojo.require("dijit.layout.StackContainer");
-  
-  
-  
-     var map, urlObjects;
-	 
-	 function initMap() {
-       patchID();
-      
-       if(configOptions.geometryserviceurl && location.protocol === "https:"){
-         configOptions.geometryserviceurl = configOptions.geometryserviceurl.replace('http:','https:');
-       }
-       esri.config.defaults.geometryService = new esri.tasks.GeometryService(configOptions.geometryserviceurl);
-
-       if(!configOptions.sharingurl){
-         configOptions.sharingurl = location.protocol + '//' + location.host + "/sharing/content/items";
-       }
-       esri.arcgis.utils.arcgisUrl = configOptions.sharingurl;
-       
-       if(!configOptions.proxyurl){   
-         configOptions.proxyurl = location.protocol + '//' + location.host + "/sharing/proxy";
-       }
-
-       esri.config.defaults.io.proxyUrl =  configOptions.proxyurl;
-
-       esri.config.defaults.io.alwaysUseProxy = false;
-      
-       urlObject = esri.urlToObject(document.location.href);
-       urlObject.query = urlObject.query || {};
-      
-       if(urlObject.query.title){
-         configOptions.title = urlObject.query.title;
-       }
-       if(urlObject.query.subtitle){
-         configOptions.title = urlObject.query.subtitle;
-       }
-       if(urlObject.query.webmap){
-         configOptions.webmap = urlObject.query.webmap;      
-       } 
-       if(urlObject.query.bingMapsKey){
-         configOptions.bingmapskey = urlObject.query.bingMapsKey;      
-       }
-
-	   esri.arcgis.utils.arcgisUrl = "http://arcgis.com/sharing/content/items";	
-		
-	   var mapDeferred = esri.arcgis.utils.createMap(configOptions.webmap, "map", {
-         mapOptions: {
-           slider: true,
-           nav: false,
-           wrapAround180:true
-         },
-         ignorePopups:false,
-         bingMapsKey: configOptions.bingmapskey
-       });
-
-       mapDeferred.addCallback(function (response) {
-        
-		 document.title = configOptions.title|| response.itemInfo.item.title || "";
-         dojo.byId("title").innerHTML = configOptions.title ||response.itemInfo.item.title;
-         dojo.byId("subtitle").innerHTML = configOptions.subtitle|| response.itemInfo.item.snippet || "";
-        
-         map = response.map;
-		
-		 dojo.connect(map,"onUpdateEnd",hideLoader);
-		
-         var layers = response.itemInfo.itemData.operationalLayers;
-         if(map.loaded){
-           initUI(layers);
-         }
-         else{
-           dojo.connect(map,"onLoad",function(){
-             initUI(layers);
-           });
-         }
-         //resize the map when the browser resizes
-         dojo.connect(dijit.byId('map'), 'resize', map,map.resize);
-       });
-
-       mapDeferred.addErrback(function (error) {
-         alert("Unable to create map: " + " " + dojo.toJson(error.message));
-       });
-	   
-     }
-
-
-     function initUI(layers) {
-       //add chrome theme for popup
-       dojo.addClass(map.infoWindow.domNode, "chrome");
-       //add the scalebar 
-       var scalebar = new esri.dijit.Scalebar({
-         map: map,
-         scalebarUnit:"english" //metric or english
-       }); 
-
-       var layerInfo = buildLayersList(layers);
-      
-       if(layerInfo.length > 0){
-         var legendDijit = new esri.dijit.Legend({
-           map:map,
-           layerInfos:layerInfo
-         },"legendDiv");
-         legendDijit.startup();
-       }
-       else{
-         dojo.byId('legendDiv').innerHTML = '';
-       }
-     }
-	 
-function buildLayersList(layers) {
-    var layerInfo = [];
-    dojo.forEach(layers, function (mapLayer, index) {
-      if (mapLayer.featureCollection && !mapLayer.layerObject) {
-        if (mapLayer.featureCollection.layers && mapLayer.featureCollection) {
-          if (mapLayer.featureCollection.layers.length === 1) {
-            layerInfo.push({
-              "layer": mapLayer.featureCollection.layers[0].layerObject,
-              "title": mapLayer.title
-            });
-          } else {
-            dojo.forEach(mapLayer.featureCollection.layers, function (layer) {
-              layerInfo.push({
-                layer: layer.layerObject,
-                title: mapLayer.title
-              });
-            });
-          }
-        }
-      } else if (mapLayer.layerObject) {
-        layerInfo.push({
-          layer: mapLayer.layerObject,
-          title: mapLayer.title
-        });
-      }
-    });
-    return layerInfo;
-  }
-    
-     function patchID() {  //patch id manager for use in apps.arcgis.com
-       esri.id._isIdProvider = function(server, resource) {
-       // server and resource are assumed one of portal domains
- 
-       var i = -1, j = -1;
- 
-       dojo.forEach(this._gwDomains, function(domain, idx) {
-         if (i === -1 && domain.regex.test(server)) {
-           i = idx;
-         }
-         if (j === -1 && domain.regex.test(resource)) {
-           j = idx;
-         }
-       });
- 
-       var retVal = false;
-   
-       if (i > -1 && j > -1) {
-         if (i === 0 || i === 4) {
-           if (j === 0 || j === 4) {
-             retVal = true;
-           }
-         }
-         else if (i === 1) {
-           if (j === 1 || j === 2) {
-             retVal = true;
-           }
-         }
-         else if (i === 2) {
-           if (j === 2) {
-             retVal = true;
-           }
-         }
-         else if (i === 3) {
-           if (j === 3) {
-             retVal = true;
-           }
-         }
-       }
- 
-       return retVal;
-     };    
+var foods = {
+    "turkey" : {
+        "layerName" : "turkey",
+        "linkText" : "Turkey",
+        "description" : "Turkey farmers want to be where there the corn and soybeans are. Geographically, then, big turkey producers are located near to processing plants and the cheap foods that will feed their livestock (Which explains the dots few and far between in regions like Utah and Texas).<br><br>There is also a large and vibrant industry of small scale production, as the smattering of dots on the map indicate. In fact, it’s not unusual to have turkey farms with a relatively small number of hogs and small-scale beef production too."
+    },
+    "sweetPotatoes" : {
+        "layerName" : "potatoes",
+        "linkText" : "Sweet Potatoes",
+        "description" : "Like cranberries, sweet potatoes are picky and require specific conditions to yield the best crops. They need a long growing season, heat in the summer and a ton of water. For this reason sweet potatoes have the best yield in the south. They were first grown in South America, though they are often confused with the white starchy yam originating from West Africa and Asia—especially during the holidays."
+    },
+    "cranberries" : {
+        "layerName" : "cranberries",
+        "linkText" : "Cranberries",
+        "description" : "Cranberries require very specific growing conditions. Because they are traditionally grown in natural wetlands, they need a lot of water. They also require a period of dormancy during the winter months which limits the regions in which they are grown to the northern parts of America."
+    },
+    "greenbeans" : {
+        "layerName" : "greenbeans",
+        "linkText" : "Green Beans",
+        "description" : "Though the map indicates that green bean farms are evenly scattered throughout a large part of the country, in the regions with the highest production—the south and the midwest for example—most of the production is driven by the location of the processing industries."
     }
-	
-	function hideLoader(){
-	  $("#loadingCon").hide();
-	}
-	
-	//Jquery Layout
-	$(document).ready(function(e) {
-	  $("#legendToggle").click(function(){
-		if ($("#legendDiv").css('display')=='none'){
-		  $("#legTogText").html('MAP LEGEND ▲');
-		}
-		else{
-		  $("#legTogText").html('MAP LEGEND ▼');
-		}
-		$("#legendDiv").slideToggle();
-	  });
+};
+
+var initApp = function(){
+    $("#description").html(foods.turkey.description);
+    dojo.forEach(getLayerByName(map,"thanksgiving",true,false),function(lyr){
+        lyr.setOpacity(0);
+        lyr.hide();
     });
+    dojo.forEach(getLayerByName(foods.turkey.layerName,true,false),function(lyr,i){
+        fadeLayerIn(map,lyr);
+    });
+};
+
+var changeFood = function(food){
+    $("#description").html(foods[food].description);
+    startFade(getLayerByName(map,food));
+};
+    
+var getLayerByName = function(mapVariable,layerName,searchMainLayers,searchGraphicsLayers){
+    var layers = [];
+        
+    if(searchMainLayers !== false){
+        dojo.forEach(mapVariable.layerIds,function(lyr){
+            if(lyr.toLowerCase().search(layerName.toLowerCase()) !== -1){
+                layers.push(mapVariable.getLayer(lyr));
+            }
+        });
+    }
+    if(searchGraphicsLayers !== false){
+        dojo.forEach(mapVariable.graphicsLayerIds,function(lyr){
+            if(lyr.toLowerCase().search(layerName.toLowerCase()) !== -1){
+                layers.push(mapVariable.getLayer(lyr));
+            }
+        });
+    }
+        
+    return layers;
+};
+    
+var startFade = function(layers){
+    dojo.forEach(getLayerByName(map,"thanksgiving",true,false),function(lyr){
+        lyr.fading = false;
+        if ($.inArray(lyr,layers) !== -1) {
+            setTimeout(function() {
+                lyr.fading = true;
+                fadeLayerIn(map,lyr);
+            }, 11);
+        }
+        else{
+            setTimeout(function() {
+                lyr.fading = true;
+                fadeLayerOut(map,lyr);
+            }, 11);
+        }
+    });
+};
+    
+var fadeLayerIn = function(mapVariable,layer){
+    if(!layer.visible){
+        layer.show();
+    }
+    if(layer.opacity < 1 && layer.fading === true){
+        layer.setOpacity(layer.opacity + 0.1);
+        setTimeout(function() {
+            fadeLayerIn(mapVariable,layer);
+        }, 10);
+    }
+    else{
+        layer.setOpacity(1);
+        layer.fading = false;
+    }
+};
+    
+var fadeLayerOut = function(mapVariable,layer){
+    if(layer.opacity > 0 && layer.fading === true){
+        layer.setOpacity(layer.opacity - 0.1);
+        setTimeout(function() {
+            fadeLayerOut(mapVariable,layer);
+        }, 10);
+    }
+    else{
+        layer.setOpacity(0);
+        layer.hide();
+        layer.fading = false;
+    }
+};
